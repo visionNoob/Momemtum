c6b59d5579654a69fcb60fa103bf018deb1d91c9
visionNoob <insurgent92@gmail.com>
2023-02-05 03:45:16 +0900

    fix

--- js/background.js
+++ js/background.js
@@ -1,14 +1,14 @@
 bg_list = [
-	"http://wallpaperswide.com/download/seoul-wallpaper-1920x1080.jpg",
-	"http://wallpaperswide.com/download/cat_63-wallpaper-1920x1080.jpg",
-	"http://wallpaperswide.com/download/vintage_bike-wallpaper-1920x1080.jpg",
-	"http://wallpaperswide.com/download/journey_painting-wallpaper-1920x1080.jpg",
-	"http://wallpaperswide.com/download/kate_upton_summertime-wallpaper-1920x1080.jpg",
-	"http://wallpaperswide.com/download/city_reflection_night-wallpaper-1920x1080.jpg",
-	"http://wallpaperswide.com/download/maldive_islands_resort-wallpaper-1920x1080.jpg",
-	"http://wallpaperswide.com/download/tokyo_tower_night_city-wallpaper-1920x1080.jpg",
-	"http://wallpaperswide.com/download/new_york_city_buildings-wallpaper-1920x1080.jpg",
-	"http://wallpaperswide.com/download/beautiful_singapore_reflection-wallpaper-1920x1080.jpg",
+	"./imgs/seoul-wallpaper-1920x1080.jpg",
+	"./imgs/cat_63-wallpaper-1920x1080.jpg",
+	"./imgs/vintage_bike-wallpaper-1920x1080.jpg",
+	"./imgs/journey_painting-wallpaper-1920x1080.jpg",
+	"./imgs/kate_upton_summertime-wallpaper-1920x1080.jpg",
+	"./imgs/city_reflection_night-wallpaper-1920x1080.jpg",
+	"./imgs/maldive_islands_resort-wallpaper-1920x1080.jpg",
+	"./imgs/tokyo_tower_night_city-wallpaper-1920x1080.jpg",
+	"./imgs/new_york_city_buildings-wallpaper-1920x1080.jpg",
+	"./imgs/beautiful_singapore_reflection-wallpaper-1920x1080.jpg",
 ];
 
 random_index = Math.floor(Math.random() * bg_list.length);
